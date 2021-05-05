cc7511574edb09339b6d7c993c915688fcc6be6d
KAM <ar-korolev@yandex.ru>
2021-05-05 15:56:25 +0300

    Стили 10

--- resources/views/web/include/numbs_line.blade.php
+++ resources/views/web/include/numbs_line.blade.php
@@ -2,9 +2,9 @@
 
 
     @foreach ($all_opo as $this_opo)
-        <a href="/opo/{{$this_opo->idOPO}}/main">
-    <div class="numbs_single good">{{$this_opo->descOPO}}<span>{{$this_opo->opo_to_calc1->first()->ip_opo}}</span></div>
-        </a>
+
+    <div class="numbs_single good"><a href="/opo/{{$this_opo->idOPO}}/main">{{$this_opo->descOPO}}<span>{{$this_opo->opo_to_calc1->first()->ip_opo}}</span></a></div>
+
     @endforeach
 {{--    <div class="numbs_single good active">Фонд скважин <span>0.98</span></div>--}}
 {{--    <div class="numbs_single good">УКПГ-4 <span>0.98</span></div>--}}
