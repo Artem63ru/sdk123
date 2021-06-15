0fc1cd9dfbefd357f9afaecb544b1831d122a834
KAM <ar-korolev@yandex.ru>
2021-06-15 15:21:46 +0300

    Отчеты

--- resources/views/web/layouts/app_admin.blade.php
+++ resources/views/web/layouts/app_admin.blade.php
@@ -14,7 +14,7 @@
     <meta property="og:description" content=""/>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+{{--    <script src="{{ asset('js/app.js') }}" defer></script>--}}
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
